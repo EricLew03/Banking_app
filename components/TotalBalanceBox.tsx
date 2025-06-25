a5ecd5879d9e4fda44b664bb47634efd388dc3af
Eric <ericlew35@gmail.com>
2025-06-24 17:39:28 -0700

    completed some components

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,8 @@
+import { formatAmount } from '@/lib/utils'
 import React from 'react'
+import CountUp from 'react-countup'
+import AnimatedCounter from './AnimatedCounter'
+import DougnutCHart from './DougnutCHart'
 
 export const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -6,17 +10,25 @@ export const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/* DoughnutChart */}
+            <DougnutCHart accounts={accounts}/>
         </div>
         
-        <div className="bg-red-500 text-white p-4 text-xl">
-  Tailwind test
-</div>
-
-        <div className='flex flex-col gap-6'>
+           <div className='flex flex-col gap-6'>
             <h2 className='header-2'>
                 Bank Accounts : {totalBanks}
             </h2>
+
+            <div className=' flex flex-col gap-2'>
+              <p className=' total-balance-label'>
+    
+                Total Current Balance
+              </p>
+
+              <div className=' total-balance-amount flex center gap-2'>
+
+                <AnimatedCounter amount = {totalCurrentBalance}/>
+              </div>
+            </div>
         </div>
     </section>
   )
