ef7e101c5cca56292d485fb2be48aaa23299f5ab
Eric <ericlew35@gmail.com>
2025-06-24 19:39:20 -0700

    worked on the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,14 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import { sidebarLinks } from '@/constants';
 import React from 'react'
+import {cn} from '@/lib/utils'
+import {usePathname} from 'next/navigation'
 
 const Sidebar = ({user}: SiderbarProps) => {
+
+    const pathname = usePathname();
   return (
     <section className = 'sidebar'> 
         <nav className=' flex flex-col gap-4'>
@@ -21,7 +26,26 @@ const Sidebar = ({user}: SiderbarProps) => {
                 max-xl:size-14'
                 />
 
+                <h1 className='sidebar-logo'>
+                    Bread Box
+                </h1>
+
             </Link>
+
+        {sidebarLinks.map((item) => {
+            const isActive = pathname === item.route || pathname.startsWith('${item.route}/'); 
+
+             return (
+             <Link
+                href={item.route}
+                key={item.label}
+                className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}>
+                
+                {item.label}
+            </Link>
+             );
+            })}
+
         </nav>
 
     </section>
