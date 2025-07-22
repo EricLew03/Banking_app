bcb971cde4589f2a403d9ce355cf037c6ab57796
Eric <ericlew35@gmail.com>
2025-07-22 16:36:57 -0700

    finished the plaid and dwolla set up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,14 +1,12 @@
-'use client'
-import React, { useState } from 'react'
-import Link from 'next/link'
+'use client';
+
 import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
-
-
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -20,183 +18,157 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { email } from 'zod/v4-mini'
-import CustomInput from './CustomInput'
-import { authformSchema } from '@/lib/utils'
-import { AArrowUp, Loader2 } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
-import PlaidLink from './PlaidLink'
+import CustomInput from './CustomInput';
+import { authformSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
+const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const formSchema = authformSchema(type);
 
-const AuthForm = ({type}: {type:string}) => {
-
-    const router = useRouter();
-
-    const [user, setUser] = useState(null);
-    const [isloading, setIsloading] = useState(false);
-    
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        email: "",
+        password: ''
+      },
+    })
    
-    
-    const formSchema = authformSchema(type);
-
-      // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: ""
-    },
-  })
-
-
     // 2. Define a submit handler.
-  const onSubmit= async(data: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setIsloading(true)
-    try {
-        if (type === 'sign-up'){
-            const newUser = await signUp(data);
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+      setIsLoading(true);
 
-            setUser(newUser);
-
-            }
+      try {
+        // Sign up with Appwrite & create plaid token
         
+        if(type === 'sign-up') {
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email,
+            password: data.password
+          }
 
-        if (type ==='sign-in') {
-            const response = await signIn({email: data.email, password: data.password});
+          const newUser = await signUp(userData);
 
-            if (response) router.push('/');
+          setUser(newUser);
         }
-    
-        
-    } catch  (error) {
+
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email,
+            password: data.password,
+          })
+
+          if(response) router.push('/')
+        }
+      } catch (error) {
         console.log(error);
-
-  } finally {
-    setIsloading(false);
-  }
-}
-
+      } finally {
+        setIsLoading(false);
+      }
+    }
 
   return (
-    <section className='auth-form'>
-        <header className='flex flex-col gap-5 md:gap-8'>
-             <Link
-                    href="/"
-                    className='cursor-pointer flex items-center gap-2'>
+    <section className="auth-form">
+      <header className='flex flex-col gap-5 md:gap-8'>
+          <Link href="/" className="cursor-pointer flex items-center gap-1">
+            <Image 
+              src="/icons/logo2.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Bread Box </h1>
+          </Link>
 
-                    <Image
-                        src="/icons/logo2.svg"
-                        width={40}
-                        height={50}
-                        alt="logo"
-                        className='size-[30px]
-                max-xl:size-14 mt-1'
-                    />
-
-                    <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>
-                        Bread Box
-                    </h1>
-
-                </Link>
-
-                <div className='flex flex-col gap-1 md:gap-3'>
-                    <h1 className= 'text-24 lg:text-36 font-semibold text-gray-900'>
-                        {user
-                        ? 'Link Account'
-                        : type === 'sign-in'
-                            ? 'Sign in'
-                            : 'Sign up'}
-
-                           <p className='text-16 font-normal text-gray-600'>
-                            {user
-                            ? 'Link your account to get started'
-                            : "Please enter your details"
-                            }
-                            </p> 
-                    </h1>
-                </div>
-        </header>
-        {user ? (
-        <div className=' flex flex-col gap-4'>
-            <PlaidLink user = {user} variant = 'primary'  />
+          <div className="flex flex-col gap-1 md:gap-3">
+            <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+              {user 
+                ? 'Link Account'
+                : type === 'sign-in'
+                  ? 'Sign In'
+                  : 'Sign Up'
+              }
+              <p className="text-16 font-normal text-gray-600">
+                {user 
+                  ? 'Link your account to get started'
+                  : 'Please enter your details'
+                }
+              </p>  
+            </h1>
+          </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
         </div>
-  ): (
-
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
-        {type ==='sign-up' && (
-            <>
-
-            <div className=' flex gap-5'>
-                   <CustomInput control = {form.control} name= 'firstName' label = 'First Name'  placeholder='Enter your first name'/>
-               <CustomInput control = {form.control} name= 'lastName' label = 'Last Name'  placeholder='Enter your last name'/>
-            </div>
-           
-                <CustomInput control = {form.control} name= 'address1' label = 'Address'  placeholder='Enter your address'/>
-                <CustomInput control = {form.control} name= 'city' label = 'City'  placeholder='Enter your city'/>
-
-                <div className='flex gap-5'>
-                  <CustomInput control = {form.control} name= 'state' label = 'State'  placeholder='ex: BC'/>
-                  <CustomInput control = {form.control} name= 'postalCode' label = 'Postal Code'  placeholder='ex: V6T 1T1'/>
-                </div>
-
-                <div className='flex gap-5'>
-                       <CustomInput control = {form.control} name= 'dateOfBirth' label = 'Date of Birth'  placeholder='yyyy-mm-dd'/>
-                    <CustomInput control = {form.control} name= 'ssn' label = 'SSN'  placeholder='ex: 1234'/>
-                </div>
-               
-                
-               </>
-        )}
-        
-        <CustomInput control = {form.control} name= 'email' label = 'Email'  placeholder='Enter your email'/>
-
-        <CustomInput control = {form.control} name= 'password' label = 'Password'  placeholder='Enter your password'/>
-
-
-        <div className=' flex flex-col gap-4'>
-            <Button className='form-btn' type="submit" disabled = {isloading}>
-            {isloading? (
+      ): (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === 'sign-up' && (
                 <>
-                <Loader2 size={20} className="animate-spin" />
-                    &nbsp; Loading...
-
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
+                  </div>
+                  <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
+                  <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: BC' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: V6T 1A1' />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
+                    <CustomInput control={form.control} name='ssn' label="SSN" placeholder='Example: 1234' />
+                  </div>
                 </>
-             ) : type === 'sign-in'
-                ? "Sign in" : 'Sign up'
+              )}
 
-            }
-        </Button>
-        </div>
-      </form>
-    </Form>
-  
-            
-    
-  )
-}
+              <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
 
-<footer className='flex justify-center gap-1'>
-    <p className='text-14 font-normal text-gray-600'>
-        {type ==='sign-in'
-        ?"Don't have an account?"
-        : "Already have an account?"}
-    </p>
+              <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
 
-    <Link href = {type  === 'sign-in' ? '/sign-up'
-        :'/sign-in'}
-     className='font-link'>
-        {type === 'sign-in' ? 'Sign up' : 'Sign in'}
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === 'sign-in' 
+                    ? 'Sign In' : 'Sign Up'}
+                </Button>
+              </div>
+            </form>
+          </Form>
 
-    </Link>
-
-</footer>
-
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === 'sign-in'
+              ? "Don't have an account?"
+              : "Already have an account?"}
+            </p>
+            <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
+              {type === 'sign-in' ? 'Sign up' : 'Sign in'}
+            </Link>
+          </footer>
+        </>
+      )}
     </section>
   )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,22 @@
 'use server'
 
 import { Account, ID, Query } from "node-appwrite";
-import { parseStringify } from "../utils";
+import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parse } from "path";
 import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid";
 import { plaidClient } from '@/lib/plaid';
+import { addFundingSource, createDwollaCustomer } from "./dwolla.actions";
+import { revalidatePath } from "next/cache";
+
+
+const {
+  APPWRITE_DATABASE_ID: DATABASE_ID,
+  APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+  APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+} = process.env;
+
 
 
 export const signIn = async ({ email, password }: signInProps) => {
@@ -34,10 +44,13 @@ export const signIn = async ({ email, password }: signInProps) => {
 export const signUp = async ({ password, ...userData }: SignUpParams) => {
   const { email, firstName, lastName } = userData;
 
-  try {
-    const { account } = await createAdminClient();
+    let newUserAccount;
 
-    const newUserAccount = await account.create(
+
+  try {
+    const { account, database } = await createAdminClient();
+
+      newUserAccount = await account.create(
       ID.unique(),
       email,
       password,
@@ -45,6 +58,28 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
     );
 
     if (!newUserAccount) throw new Error("Error creating user");
+
+      const dwollaCustomerUrl = await createDwollaCustomer({
+      ...userData,
+      type: 'personal'
+    })
+
+    if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
+
+    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+
+    const newUser = await database.createDocument(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...userData,
+        userId: newUserAccount.$id,
+        dwollaCustomerId,
+        dwollaCustomerUrl
+      }
+    )
+
 
     const session = await account.createEmailPasswordSession(email, password);
 
